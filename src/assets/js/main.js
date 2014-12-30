118ecda08ab5679d0e431e05f896acd22f2bcb16
NomaanA <Nomaan.Ahgharian@mail.rit.edu>
2014-12-29 23:27:57 -0500

    Added Schedule page
    Added Schedule page - retrieving information from google Cal - read
    only for now

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,34 @@
-$(function() {
-	// $('.js-lazyYT').lazyYT(); 
-	autoPlayYouTubeModal();
+$(document).ready(function() {
+
+    autoPlayYouTubeModal();
+    $('#calendar').fullCalendar({
+        weekends : false,
+        googleCalendarApiKey: 'AIzaSyAWRBsQtQUXh7_lPIQO5qma8fwDCR6Zmrc',
+        events: {
+            googleCalendarId: 'rhm51ep2l0lu0igfjtd03iif30@group.calendar.google.com',
+            color: 'yellow',   // an option!
+            textColor: 'black' // an option!
+        },
+
+
+    });
 });
+
+
 
 function autoPlayYouTubeModal(){
   var trigger = $("body").find('[data-toggle="modal"]');
   trigger.click(function() {
     var theModal = $(this).data( "target" ),
-    videoSRC = $(this).attr( "data-theVideo" ), 
+    videoSRC = $(this).attr( "data-theVideo" ),
     videoSRCauto = videoSRC+"?autoplay=1" ;
     $(theModal+' iframe').attr('src', videoSRCauto);
     $(theModal+' button.close').click(function () {
         $(theModal+' iframe').attr('src', videoSRC);
-    }); 
+    });
     $(theModal).click(function () {
         $(theModal+' iframe').attr('src', videoSRC);
-    });    
+    });
   });
 }
+
